bb915154e3d01c0c615a3df9b91debcb03d26b29
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-05-11 17:49:36 +0400

    build(all doc) end some task

---
+++ some.js
@@ -0,0 +1,20 @@
+// SOME
+if (!Array.prototype.newEvery) {
+  Array.prototype.newEvery = function (calBack) {
+    let arr = this;
+    if (typeof calBack === "function") {
+      for (let index = 0; index < arr.length; index++) {
+        if (calBack(arr[index], index, arr)) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return "is not function";
+    }
+  };
+}
+
+let arr = [1, 2, 3, 4];
+
+console.log(arr.newEvery((a) => a === 2));
